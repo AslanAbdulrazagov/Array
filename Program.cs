00c3b9055212d8b3213c5e0c093f073afc1158d5
AslanAbdulrazagov <aslanabdulrazaqov496@gmail.com>
2024-05-23 18:35:18 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region reverse array
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 8, 9, 10 };
+            //int arraylength = arr.Length;
+            //int temp = 0;
+            //for (int i = 0; i < arr.Length / 2; i++)
+            //{
+            //    temp = arr[i];
+            //    arr[i] = arr[arraylength - i - 1];
+            //    arr[arraylength - i - 1] = temp;
+
+
+            //}
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+            #endregion
+
+            #region two arrays
+            //int[] firstArr= {1,2,4,3,5};
+            //int[] secondArr = { 1, 2, 3,5,4,6,7};
+            //bool isEqual = true;
+            //for (int i = 0; i < firstArr.Length; i++) {
+            //    if (isEqual)
+            //    {
+
+            //        for (int j = 0; j < secondArr.Length; j++)
+            //        {
+            //            if (firstArr[i] == secondArr[j])
+            //            {
+            //                isEqual = true;
+            //                break;
+            //            }
+            //            else
+            //            {
+            //                isEqual = false;
+            //            }
+            //        }
+            //    }
+            //    else
+            //    {
+            //        isEqual = false;
+            //        break;
+            //    }
+
+
+
+
+            //}
+            //Console.WriteLine(isEqual);
+            #endregion
+
+            #region dublicate 
+            //int[] arr= { 1, 2, 3, 4, 5,2,6 };
+            //bool isDublicated = false;
+            //for (int i = 0; i < arr.Length-1; i++)
+            //{
+            //    if (arr[i] == arr[i + 1])
+            //    {
+            //        isDublicated=true;
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine(isDublicated);
+
+
+            #endregion
+        }
+    }
+}
